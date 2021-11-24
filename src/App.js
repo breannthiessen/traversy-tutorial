e62916fc8847e93f4ef72769bf00aaf328ba44a2
Breann T <thiessenbreann@gmail.com>
2021-11-24 10:28:29 -0700

    init

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from './components/Header'
 import { Tasks } from './components/Tasks';
 import AddTask from './components/AddTask';
 import Footer from './components/Footer';
-import About from './components/About'
 
 const serverAddress = 'http://localhost:5000/tasks'
 
